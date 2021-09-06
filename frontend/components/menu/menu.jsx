3fa532414f91dfba7edd79f9796213ae74276e95
David Woolner <davidwoolner@gmail.com>
2021-09-05 22:51:54 -0400

    Add dropdown menu.

--- frontend/components/menu/menu.jsx
+++ frontend/components/menu/menu.jsx
@@ -9,20 +9,61 @@ import { AuthRoute } from '../../util/route_util';
 class Menu extends React.Component {
     constructor(props){
         super(props);
+
+        this.menu = React.createRef();
+        this.state = {
+            revealDropdown: false,
+        }
+
+    }
+
+    handleButtonClick = () => {
+        this.setState(({revealDropdown}) => {
+            return {
+                revealDropdown: !revealDropdown
+            }
+        })
+    }
+
+    handleClickOutside = e => {
+        if (
+            this.menu.current &&
+            !this.menu.current.contains(e.target)
+        ) {
+            this.setState({
+                revealDropdown: false,
+            });
+        }
+    }
+
+    componentDidMount() {
+        document.addEventListener("mousedown", this.handleClickOutside)
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener("mousedown", this.handleClickOutside)
     }
 
     render() {
 
         return (
-            <div className='menu-btn'>
-                <h4>Menu</h4>
-                <div className="menu-dropdown">
-                    <Link to='/'>Login</Link>
-                    <Link to='/'>Signup</Link>
-                    {/* <AuthRoute exact path="/login" component={LoginFormContainer} />
-                    <AuthRoute exact path="/signup" component={SignUpFormContainer} /> */}
+                <div onClick={this.handleButtonClick} className="menu" ref={this.menu}>
+                    {/* <button >Menu</button> */}
+                    <h3>Menu</h3>           
+                    { this.state.revealDropdown && (         
+                            <>
+                                <div className="menu-content">
+                                    <Link to='/'>Login</Link>
+                                    <Link to='/'>Signup</Link>
+                                </div>
+                                <div onClick={this.hideDropdown} className="click-bg"></div>
+                             </> 
+
+                    )}
+
+                        {/* <AuthRoute exact path="/login" component={LoginFormContainer} />
+                        <AuthRoute exact path="/signup" component={SignUpFormContainer} /> */}
                 </div>
-            </div>
         )
     }
 }
