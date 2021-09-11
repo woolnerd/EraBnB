48755cc40f48ece3d70e0725b5be46a16075310e
David Woolner <davidwoolner@gmail.com>
2021-09-11 01:35:08 -0400

    Add more styles to listings index.

--- frontend/components/listings/listing_index_item.jsx
+++ frontend/components/listings/listing_index_item.jsx
@@ -1,20 +1,27 @@
 import React from "react";  
 import { Link } from 'react-router-dom';
 
-export const ListingIndexItem = ({listing}) => (
-
+export const ListingIndexItem = ({ listing }) => (
   <div className="listing-item">
-      <Link to={`/listings/${listing.id}`}>
-        <img src={listing.photoUrl[0]} alt="" />
-        </Link>
-      <div className="listing-info">
+    <Link to={`/listings/${listing.id}`}>
+      <img src={listing.photoUrl[0]} alt="" />
+    </Link>
+    <div className="listing-info">
+      <span>
+        <p>Entire unit in Brooklyn</p>
         <h3>{listing.title}</h3>
-        <p>{listing.description}</p>
-        <p>{listing.address}</p>
-        <p>${listing.price}/night</p>
-        <p>Bedrooms {listing.num_bedrms} <span> Bathrooms {listing.num_baths}</span></p>
-      </div>
+      </span>
+      <p>{listing.description}</p>
+      <p>{listing.address}</p>
+      <p>
+        Bedrooms {listing.num_bedrms}{" "}
+        <span> Bathrooms {listing.num_baths}</span>
+      </p>
+      <span className="item-price">
+        <p className="special">${listing.price}/night</p>
+      </span>
     </div>
-)
+  </div>
+);
 
 // 
