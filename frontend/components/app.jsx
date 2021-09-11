9c7d2cf2e72d11a211f0b367d3cb18615716833e
David Woolner <davidwoolner@gmail.com>
2021-09-11 00:15:40 -0400

    Extract splash from app.jsx in to splash.jsx.

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -26,105 +26,14 @@ const App = () => (
     <Modal />
     <header className="header-container">
       <Link to="/">
-        <img src="https://img.icons8.com/ios/50/000000/airbnb.png" />
+        <img className="airbnb-icon" src="https://img.icons8.com/ios/50/000000/airbnb.png" />
       </Link>
       <button className="search-btn">Start your search</button>
       <MenuContainer />
     </header>
-    <Route exact path="/" component={Splash} />
-    <div className="main-content">
-      <h2>Explore nearby</h2>
-      <div className="grid-container">
-        <div className="grid-thumbs">
-          <div></div>
-          <span>
-            <h3>New York</h3>
-            <h4>2 hour drive</h4>
-          </span>
-        </div>
-        <div className="grid-thumbs">
-          <div></div>
-          <span>
-            <h3>New York</h3>
-            <h4>2 hour drive</h4>
-          </span>
-        </div>
-        <div className="grid-thumbs">
-          <div></div>
-          <span>
-            <h3>New York</h3>
-            <h4>2 hour drive</h4>
-          </span>
-        </div>
-        <div className="grid-thumbs">
-          <div></div>
-          <span>
-            <h3>New York</h3>
-            <h4>2 hour drive</h4>
-          </span>
-        </div>
-        <div className="grid-thumbs">
-          <div></div>
-          <span>
-            <h3>New York</h3>
-            <h4>2 hour drive</h4>
-          </span>
-        </div>
-        <div className="grid-thumbs">
-          <div></div>
-          <span>
-            <h3>New York</h3>
-            <h4>2 hour drive</h4>
-          </span>
-        </div>
-        <div className="grid-thumbs">
-          <div></div>
-          <span>
-            <h3>New York</h3>
-            <h4>2 hour drive</h4>
-          </span>
-        </div>
-        <div className="grid-thumbs">
-          <div></div>
-          <span>
-            <h3>New York</h3>
-            <h4>2 hour drive</h4>
-          </span>
-        </div>
-      </div>
-      <h2>Live anywhere</h2>
-      <div className="large-thumbs-container">
-        <div>
-          <div className="large-thumbs"></div>
-          <h3>Unique stays</h3>
-        </div>
-        <div>
-          <div className="large-thumbs"></div>
-          <h3>Unique stays</h3>
-        </div>
-        <div>
-          <div className="large-thumbs"></div>
-          <h3>Unique stays</h3>
-        </div>
-        <div>
-          <div className="large-thumbs"></div>
-          <h3>Unique stays</h3>
-        </div>
-      </div>
-      <div className="lower-third">
-        <span>
-          <h3>Try Hosting</h3>
-        </span>
-        <span>
-          <p>
-            Earn extra income and unlock new opportunities by sharing your
-            space.
-          </p>
-        </span>
-        <span>
-          <button>Learn more</button>
-        </span>
-      </div>
+
+      <Route exact path="/" component={Splash} />
+
       <Route exact path="/listings" component={ListingsIndexContainer} />
       <Route exact path={`/listings/:listingId`} component={ListingContainer} />
       {/* <AuthRoute exact path="/signup" component={SignUpFormContainer} /> */}
@@ -135,7 +44,6 @@ const App = () => (
         component={ListingFormContainer}
       />
       <ProtectedRoute exact path="/users/:userId" component={UsersContainer} />
-    </div>
   </div>
 );
 
--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -2,17 +2,109 @@ import React from "react";
 import { Link } from 'react-router-dom';
 
 export const Splash = () => (
-    <>
-        <Link to="/listings">
-            <main id="splash1">
-                <div>
-                    <h2>Not sure where to go? Perfect.</h2>
-                    <button className="flexible-btn">I'm flexible</button>
-                </div>
-            </main>
-        </Link>
-        <main id="splash2"></main>
-        <main id="splash3"></main>
-    </>
-)
+  <>
+    <Link to="/listings">
+      <main id="splash1">
+        <div>
+          <h2>Not sure where to go? Perfect.</h2>
+          <button className="flexible-btn">I'm flexible</button>
+        </div>
+      </main>
+    </Link>
+    <div className="main-content">
+      <h2>Explore nearby</h2>
+      <div className="grid-container">
+        <div className="grid-thumbs">
+          <div></div>
+          <span>
+            <h3>New York</h3>
+            <h4>2 hour drive</h4>
+          </span>
+        </div>
+        <div className="grid-thumbs">
+          <div></div>
+          <span>
+            <h3>New York</h3>
+            <h4>2 hour drive</h4>
+          </span>
+        </div>
+        <div className="grid-thumbs">
+          <div></div>
+          <span>
+            <h3>New York</h3>
+            <h4>2 hour drive</h4>
+          </span>
+        </div>
+        <div className="grid-thumbs">
+          <div></div>
+          <span>
+            <h3>New York</h3>
+            <h4>2 hour drive</h4>
+          </span>
+        </div>
+        <div className="grid-thumbs">
+          <div></div>
+          <span>
+            <h3>New York</h3>
+            <h4>2 hour drive</h4>
+          </span>
+        </div>
+        <div className="grid-thumbs">
+          <div></div>
+          <span>
+            <h3>New York</h3>
+            <h4>2 hour drive</h4>
+          </span>
+        </div>
+        <div className="grid-thumbs">
+          <div></div>
+          <span>
+            <h3>New York</h3>
+            <h4>2 hour drive</h4>
+          </span>
+        </div>
+        <div className="grid-thumbs">
+          <div></div>
+          <span>
+            <h3>New York</h3>
+            <h4>2 hour drive</h4>
+          </span>
+        </div>
+      </div>
+      <h2>Live anywhere</h2>
+      <div className="large-thumbs-container">
+        <div>
+          <div className="large-thumbs"></div>
+          <h3>Unique stays</h3>
+        </div>
+        <div>
+          <div className="large-thumbs"></div>
+          <h3>Unique stays</h3>
+        </div>
+        <div>
+          <div className="large-thumbs"></div>
+          <h3>Unique stays</h3>
+        </div>
+        <div>
+          <div className="large-thumbs"></div>
+          <h3>Unique stays</h3>
+        </div>
+      </div>
+      <div className="lower-third">
+        <span>
+          <h3>Try Hosting</h3>
+        </span>
+        <span>
+          <p>
+            Earn extra income and unlock new opportunities by sharing your
+            space.
+          </p>
+        </span>
+        <span>
+          <button>Learn more</button>
+        </span>
+      </div>
+    </div>
+  </>
+);
 
