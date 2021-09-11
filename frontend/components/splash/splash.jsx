4710225c3a7630a9472333d672213a5b66f3d0e7
David Woolner <davidwoolner@gmail.com>
2021-09-10 23:22:34 -0400

    Add a lot of splash layout style.

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -4,7 +4,12 @@ import { Link } from 'react-router-dom';
 export const Splash = () => (
     <>
         <Link to="/listings">
-            <main id="splash1"></main>
+            <main id="splash1">
+                <div>
+                    <h2>Not sure where to go? Perfect.</h2>
+                    <button className="flexible-btn">I'm flexible</button>
+                </div>
+            </main>
         </Link>
         <main id="splash2"></main>
         <main id="splash3"></main>
