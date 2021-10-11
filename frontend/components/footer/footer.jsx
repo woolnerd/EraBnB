3f15cba4252c979508eb0b346400b7bcc6817eea
David Woolner <davidwoolner@gmail.com>
2021-10-11 09:19:40 -0400

    Add links to footer

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -6,22 +6,48 @@ export const Footer = () => (
     <div className="footer-container">
       <ul>
         <li className="top-col">About me</li>
-        <li>Github</li>
-        <li>LinkdIn</li>
-        <li>AngelList</li>
-        <li>IMDB</li>
+        <li>
+          {" "}
+          <a href="https://github.com/DavidWoolner/" target="_blank">
+            Github
+          </a>
+        </li>
+        <li>
+          {" "}
+          <a href="https://www.linkedin.com/in/david-woolner/" target="_blank">
+            LinkdIn
+          </a>
+        </li>
+        <li>
+          {" "}
+          <a href="https://angel.co/u/david-woolner" target="_blank">
+            AngelList
+          </a>
+        </li>
         <li>Portfolio site</li>
       </ul>
       <ul>
         <li className="top-col">Contact</li>
-        <li>DavidWoolner@gmail.com</li>
+        <li>
+          <a href="mailto: davidwoolner@gmail.com">DavidWoolner@gmail.com</a>
+        </li>
         <li>(347) 882-6981</li>
         <li>NYC Metro Area</li>
       </ul>
       <ul>
         <li className="top-col">Past Projects</li>
-        <li>NYC Cinemap</li>
-        <li>MERN Project</li>
+        <li>
+          {" "}
+          <a href="https://nyc-cinemap.herokuapp.com/" target="_blank">
+            NYC Cinemap
+          </a>
+        </li>
+        <li>
+          {" "}
+          <a href="https://come-what-may.herokuapp.com/?#/" target="_blank">
+            Come What May
+          </a>
+        </li>
       </ul>
     </div>
   </div>
