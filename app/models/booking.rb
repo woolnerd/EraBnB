f273f46e02123d8d7a8718f25d9497f3a4e107d0
David Woolner <davidwoolner@gmail.com>
2021-12-22 16:52:28 -0500

    Add booking conflict backend

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,7 +4,10 @@ class Booking < ApplicationRecord
     validates :check_in, date: {before: Proc.new {Time.now + 3.year}}
     validates :check_out, date: { after: :check_in}
     validates :check_in, :check_out, date: true
-    validate :booker_id_cannot_be_host_id
+    validate  :booker_id_cannot_be_host_id
+    validate  :check_in_conflict?
+    validate  :check_out_conflict?
+
     
     belongs_to :listing,
         foreign_key: :listing_id,
@@ -23,5 +26,27 @@ class Booking < ApplicationRecord
             errors.add(:base, "Sorry, you cannot book your own listing")
         end
     end
+
+    def check_in_conflict? 
+         conflict?(check_in, "in")
+    end
     
+    def check_out_conflict?
+        conflict?(check_out, "out")
+    end
+
+    def conflict?(date, type)
+        booked_ranges = []
+        if listing.bookings.length > 0
+            listing.bookings.each do |booking|
+                        date_range = booking.check_in..booking.check_out
+                        booked_ranges << date_range
+            end 
+            if booked_ranges.any? {|range| range.include?(date)}
+                errors.add(:base, "That check #{type} date is not available")
+            end
+        end 
+        false 
+    end
+
 end
