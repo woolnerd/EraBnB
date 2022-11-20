aa56c2c7f9213d278e29b980de3bc210f916a48c
DavidWoolner <davidwoolner@voray.com>
2022-11-20 11:15:00 -0500

    Add flyio setup

--- app/controllers/api/listings_controller.rb
+++ app/controllers/api/listings_controller.rb
@@ -1,51 +1,50 @@
 class Api::ListingsController < ApplicationController
-    before_action :ensure_logged_in, only: [:create, :update, :destroy]     
+    before_action :ensure_logged_in, only: [:create, :update, :destroy]
 
-    def index 
+    def index
         @listings = Listing.with_attached_photos.all
 
         if params[:query]
-            search()
-        end 
-
-        render "api/listings/index"
+            search and return
+        end
+        render :index
     end
- 
+
     def flexible
-        listing_id = Listing.flexible()
+        listing_id = Listing.flexible
         redirect_to api_listing_url(listing_id)
     end
 
-    def create 
+    def create
         @listing = Listing.new(listing_params)
         @host = @listing.host
-        if @listing.save 
+        if @listing.save
             render :show
-        else  
+        else
             render json: @listing.errors.full_messages, status: 422
         end
     end
 
-    def show 
-            @listing = Listing.with_attached_photos.find(params[:id])
-            
-            @booked_dates =  []
-            @listing.bookings.each do |booking|
-                date_range = booking.check_in..booking.check_out
-                @booked_dates += Array(date_range)
-            end
-            @host = @listing.host
-            render :show
+    def show
+        @listing = Listing.with_attached_photos.find(params[:id])
+
+        @booked_dates =  []
+        @listing.bookings.each do |booking|
+            date_range = booking.check_in..booking.check_out
+            @booked_dates += Array(date_range)
+        end
+        @host = @listing.host
+        render :show
     end
 
-    def update 
+    def update
         @listing = Listing.find(params[:id])
         @host = @listing.host
         if cannot_edit(@listing)
             render json: @listing.errors.full_messages, status: 401
         elsif @listing && @listing.update(listing_params)
             render :show
-        else 
+        else
             render json: @listing.errors.full_messages, status: 404
         end
     end
@@ -54,16 +53,16 @@ class Api::ListingsController < ApplicationController
         @listing = Listing.find(params[:id])
         if cannot_edit(@listing)
             render json: @listing.errors.full_messages, status: 401
-        elsif @listing 
+        elsif @listing
             @listing.destroy
             @listings = Listing.with_attached_photos.all
            render :index
-        else  
+        else
             render json: @listing.errors.full_messages, status: 404
-        end 
+        end
     end
 
-    private 
+    private
 
     def cannot_edit(listing)
         listing.host_id != current_user.id
@@ -75,14 +74,10 @@ class Api::ListingsController < ApplicationController
         desired_check_out = Date.parse(params[:query][:check_out])
 
         if address != ""
-            @listings = @listings.where("address LIKE :query", query: "%"+address+"%")
-                                .or(@listings.where("address LIKE :query", query: "%"+address.downcase+"%"))
-                                .or(@listings.where("address LIKE :query", query: "%"+address.upcase+"%"))
-                                .or(@listings.where("address LIKE :query", query: "%"+address.downcase.titleize+"%"))
-        
+            @listings = @listings.where("address ILIKE :query", query: "%#{address}%")
         end
 
-        @listings = 
+        @listings =
             @listings.select do |listing|
                 booked_ranges = []
                 if listing.bookings.length > 0
@@ -90,12 +85,12 @@ class Api::ListingsController < ApplicationController
                         date_range = booking.check_in..booking.check_out
                         booked_ranges << date_range
                     end
-                end 
-                 booked_ranges.all? {|range| !range.include?(desired_check_in)} &&
-                 booked_ranges.all? {|range| !range.include?(desired_check_out)}
+                end
+                 booked_ranges.all? { |range| !range.include?(desired_check_in) } &&
+                 booked_ranges.all? { |range| !range.include?(desired_check_out) }
             end
 
-            
+
         if params[:query][:era_theme] != ""
             @listings = @listings.select {|listing| listing.era_theme == params[:query][:era_theme] }
         end
@@ -114,7 +109,7 @@ class Api::ListingsController < ApplicationController
                                         :clean_fee,
                                         :service_fee,
                                         :host_id,
-                                        photos: [] 
+                                        photos: []
                                         )
     end
 
